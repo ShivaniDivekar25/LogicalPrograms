9b52a042caa077161983fb93da2faa3e6a6bf2cd
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-08 16:40:52 +0530

    Added Logical Programs

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Welcome to logical programs");
             //Taking user input
             Console.WriteLine("Please enter option");
-            Console.WriteLine("1:Fibonacci Series\n2:Prime Number\n3:Reverse Number\n4:Perfect Number");
+            Console.WriteLine("1:Fibonacci Series\n2:Prime Number\n3:Reverse Number\n4:Perfect Number\n5:Day of week");
             int option = Convert.ToInt32(Console.ReadLine());
             //Switch case to call different method 
             switch (option)
@@ -29,6 +29,10 @@
                 case 4:
                     PerfectNumber perfectNumber = new PerfectNumber();
                     perfectNumber.PerfectNumbers();
+                    break;
+                case 5:
+                    DayOfWeek dayOfWeek = new DayOfWeek();
+                    dayOfWeek.DayOfWeeks();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
