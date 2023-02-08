a8430c6a5b247c6bc2bb928ac46612e564d3d58a
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-08 08:35:18 +0530

    Added Logical Program

---
+++ LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci(int count)
+        {
+            int p1 = 0, p2 = 1, p3;
+            Console.Write(p1 +" " + p2);
+            for(int i=1; i<count; i++)
+            {
+                p3 = p1 + p2;
+                Console.Write(" " + p3);
+                p1 = p2;
+                p2 = p3;
+            }
+        }
+    }
+}
---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical programs");
+            //Taking user input
+            Console.WriteLine("Please enter option");
+            Console.WriteLine("1:Fibonacci Series");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Switch case to call different method 
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Please enter number");
+                    int count = Convert.ToInt32(Console.ReadLine());
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    fibonacciSeries.Fibonacci(count);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
+        }
+    }
+}
